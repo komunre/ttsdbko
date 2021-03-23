df9e3472500caacde955c7ac7ac6430970a9f462
komunre <koliziy5@gmail.com>
2021-03-23 16:37:16 -0400

    All files

---
+++ src/main/java/DeckLoader.java
@@ -0,0 +1,82 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class DeckLoader {
+    String directory;
+    BufferedImage[] images = new BufferedImage[7 * 10];
+    void load(String dir){
+        directory = dir;
+        File builder = new File(directory + "builder");
+        int index = 0;
+        try {
+            Scanner scanner = new Scanner(builder);
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                String[] splitted = line.split(" ");
+                int counter = -1;
+                try {
+                    counter = Integer.parseInt(splitted[0]);
+                }
+                catch (NumberFormatException e){
+                    System.out.println("Syntax error");
+                }
+                File imageFile = new File(directory + splitted[1]);
+                try {
+                    BufferedImage image = ImageIO.read(imageFile);
+                    if (counter != 0) {
+                        for (int x = 0; x < counter; x++) {
+                            images[index] = image;
+                            index++;
+                        }
+                    }
+                    else {
+                        images[69] = image;
+                    }
+                }
+                catch (IOException e) {
+                    System.out.println("Can't read file: " + directory + imageFile);
+                }
+            }
+        }
+        catch (IOException e){
+            System.out.println("Can't read file: " + directory + "builder");
+        }
+
+        System.out.println("Exporting...");
+        int chunkWidth = images[0].getWidth();
+        int chunkHeight = images[0].getHeight();
+
+        BufferedImage finalImg = new BufferedImage(chunkWidth * 10, chunkHeight * 7, images[0].getType());
+        int num = 0;
+        for (int y = 0; y < 7; y++){
+            for (int x = 0; x < 10; x++){
+                BufferedImage image;
+                if (images[num] == null){
+                    image = new BufferedImage(chunkWidth, chunkHeight, images[0].getType());
+                    Graphics2D graphics = image.createGraphics();
+                    graphics.setPaint(new Color(0, 0, 0));
+                    graphics.fillRect(0, 0, image.getWidth(), image.getHeight());
+                }
+                else{
+                    image = images[num];
+                }
+                int finalX = chunkWidth * x;
+                finalImg.createGraphics().drawImage(image, finalX, chunkHeight * y, null);
+                num++;
+            }
+        }
+
+        try {
+            ImageIO.write(finalImg, "png", new File(directory + "deck.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Exported");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args){
+        DeckLoader loader = new DeckLoader();
+        loader.load(args[0]);
+    }
+}
